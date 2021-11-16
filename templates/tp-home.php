fe2c33125fee5e6009b1500e51a9e78f7462ee65
Jonathan Barbeau <jonathan@instynctweb.com>
2021-11-16 16:38:50 -0500

    added font

--- templates/tp-home.php
+++ templates/tp-home.php
@@ -6,6 +6,16 @@ Template Name: Home
 // Load header.php
 get_header(); ?>
 
+<section class="styleguide">
+    <div class="styleguide__container">
+        <div class="styleguide__title-container">
+            <h1>Styleguide H1</h1>
+            <h2>Titre H2</h2>
+            <h3>Titre H3</h3>
+            <h4>Titre h4</h4>
+        </div>
+    </div>
+</section>
 
 <?php
 // Load footer.php
