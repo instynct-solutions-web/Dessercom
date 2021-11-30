a288d323c89a9c661f52bbf92c6cbfcdf6dfe3d0
Jonathan Barbeau <jonathan@instynctweb.com>
2021-11-30 16:20:01 -0500

    page services done -html

--- templates/tp-services.php
+++ templates/tp-services.php
@@ -56,6 +56,85 @@ $extra = get_field('services_ext') ?>
         </div>
     </div>
 </section>
+<?php
+$paramedic = get_field('services_paramedic'); ?>
+<section class="services-paramedic">
+    <div class="services-paramedic__container">
+        <div class="services-paramedic__top">
+            <p class="services-paramedic__note"><?= $paramedic['note']; ?></p>
+        </div>
+        <div class="services-paramedic__main">
+            <div class="services-paramedic__image-container">
+                <img src="<?= $paramedic['image']['url']; ?>" alt="<?= $paramedic['image']['alt']; ?>" class="services-paramedic__image">
+            </div>
+            <div class="services-paramedic__text-container">
+                <h2 class="services-paramedic__title"><?= $paramedic['title'] ?></h2>
+                <p class="services-paramedic__text"><?= $paramedic['text'] ?></p>
+                <p class="services-paramedic__subtext"><?= $paramedic['subtext'] ?></p>
+                <p class="services-paramedic__info"><?= $paramedic['info'] ?></p>
+                <p class="services-paramedic__contact">
+                    <a href="<?= $paramedic['email']['url'] ?>" class="services-paramedic__link"><?= $paramedic['email']['title'] ?></a>
+                    <span><?= ' ' . __('ou') . ' ' ?></span>
+                    <a href="<?= $paramedic['phone']['url'] ?>" class="services-paramedic__link"><?= $paramedic['phone']['title'] ?></a>
+                </p>
+                <a href="<?= $paramedic['button']['url'] ?>" class="services-paramedic__button"><?= $paramedic['button']['title'] ?></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+$support = get_field('support') ?>
+<section class="services-support">
+    <div class="services-support__container">
+        <div class="services-support__text-container">
+            <h3 class="services-support__title"><?= $support['title'] ?></h3>
+            <p class="services-support__text"><?= $support['text'] ?></p>
+            <div class="services-support__button-container">
+                <a href="<?= $support['link']['url'] ?>" class="services-support__button"><?= $support['link']['title'] ?></a>
+                <a href="<?= $support['link_2']['url'] ?>" class="services-support__button services-support__button--secondary"><?= $support['link_2']['title'] ?></a>
+            </div>
+
+        </div>
+        <div class="services-support__animation-container"></div>
+    </div>
+</section>
+
+<?php
+$other = get_field('other') ?>
+<section class="services-other">
+    <div class="services-other__container">
+        <div class="services-other__top">
+            <h2 class="services-other__title"><?= $other['title'] ?></h2>
+            <a target="<?= $other['link']['target'] ?>" href="<?= $other['link']['url'] ?>" class="services-other__button services-other__button--desktop"><?= $other['link']['title'] ?></a>
+        </div>
+        <?php
+        if ($other['list']) { ?>
+            <ul class="services-other__list">
+                <?php
+                foreach ($other['list'] as $item) { ?>
+                    <li class="services-other__item">
+                        <div class="services-other__item-header">
+                            <h4 class="services-other__item-title"><?= $item['name'] ?></h4>
+                        </div>
+                        <div class="services-other__item-body">
+                            <p class="services-other__item-text"><?= $item['text'] ?></p>
+                        </div>
+                    </li>
+                <?php
+                } ?>
+
+            </ul>
+        <?php
+        } ?>
+
+        <div class="services-other__button-container">
+            <a target="<?= $other['link']['target'] ?>" href="<?= $other['link']['url'] ?>" class="services-other__button services-other__button--mobile"><?= $other['link']['title'] ?></a>
+        </div>
+    </div>
+</section>
+
+
 
 <?php
 // Load footer.php
