f7b7cd1f1bbb96f1362fd05bf9ed21ddbe35a4e6
Alexandre Giroux <alex@instynctweb.com>
2021-12-17 15:27:09 -0500

    Desktop

--- templates/tp-pricing.php
+++ templates/tp-pricing.php
@@ -81,14 +81,14 @@ $transport = get_field('transport') ?>
             </a>
         </div>
         <div class="pricing-transport__main">
-            <div class="pricing-transport__image-container">
-                <img src="<?= $transport['image']['url']; ?>" alt="<?= $transport['image']['alt']; ?>" class="pricing-transport__image">
-                <svg class="pricing-transport__mask" id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 335.4 302.3">
-                    <polygon class="cls-1" points="213.3 160.5 214 160.5 214 160.3 213.6 160 213.3 160.5" />
-                    <polygon class="cls-1" points="123.1 160.5 123.4 160.5 123.1 159.8 123.1 160.5" />
-                    <path class="cls-1" d="M331.2,0H0V302.2H335.4V0ZM281.1,298.1a64.19,64.19,0,0,1-25.1-8.3l-88.1-51.3.1.1L79.7,289.5c-26.2,14.9-57.6,9.2-70.1-12.7S8.3,225,34.5,210l88.6-50.4.1.1c-.1-8.7-.5-98.9-.5-101.2,0-29,18.9-52.6,42.7-54.5h6C195.1,6,214,29.6,214,58.6V160.3c6.2,3.6,85.9,49.4,87.9,50.6,17.1,10,27.6,26.3,29.3,42.5v8.8a39.78,39.78,0,0,1-5.1,15.7c-6.9,11.8-19.3,18.8-33.4,20.3H281.1Z" />
-                </svg>
-            </div>
+            <svg class="pricing-transport__image" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 181.567 163.488">
+                <defs>
+                    <pattern id="image" patternContentUnits="objectBoundingBox" width="1" height="1">
+                        <image x="0" y="0" width="1" height="1" preserveAspectRatio="xMidYMid slice" xlink:href="<?= $transport['image']['url']; ?>"></image>
+                    </pattern>
+                </defs>
+                <path id="Tracé_974" data-name="Tracé 974" d="M178.728,151.529c-6.956,12.172-24.362,15.327-38.914,7.036-1.2-.695-49.044-28.243-49.044-28.243l.049-.08L41.9,158.725c-14.49,8.4-31.927,5.4-38.963-6.686v-.031C-4.1,139.9,1.926,123.262,16.392,114.835c1.15-.64,45.384-26.1,48.834-28.077v.129H65.6l-.16-.264-.215.135V30.3C65.227,13.562,76.593,0,90.6,0s25.383,13.562,25.383,30.3c0,1.31-.24,51.375-.271,56.215l-.209.369h.209v-.369l.031-.055,49.179,28C179.478,122.751,185.653,139.333,178.728,151.529Z" transform="translate(0)" fill="url(#image)" />
+            </svg>
             <div class="pricing-transport__text-container">
                 <h2 class="pricing-transport__title"><?= $transport['title'] ?></h2>
                 <p class="pricing-transport__text"><?= $transport['text'] ?></p>
