1c7d1556ae99dbb48e3882449ed44e6a15a352e3
Jonathan Barbeau (instynct) <51244878+Jobarbeau@users.noreply.github.com>
2022-02-21 15:21:52 -0500

    Tooltips and form fix

--- assets/src/js/modules/_invoiceHint.js
+++ assets/src/js/modules/_invoiceHint.js
@@ -1,19 +1,32 @@
 export default class InvoiceHint {
 	constructor() {
 		this.hintIcon = document.querySelector('[data-hint-icon]');
-		this.hintImage = document.querySelector('[data-hint-image]');
-
-		if (this.hintImage) {
+		this.hintImageCloseList = document.querySelectorAll('[data-hint-image-close]');
+		this.hintImageContainerList = document.querySelectorAll('[data-hint-image-container]');
+		this.hintImageList = document.querySelectorAll('[data-hint-image]');
+		if (this.hintImageList) {
 			this.manageEvents();
 		}
 	}
 
 	manageEvents() {
-		this.hintIcon.addEventListener('mouseover', () => {
-			this.hintImage.classList.add(this.hintImage.classList.add(`${this.hintImage.classList[0]}--visible`));
-		});
-		this.hintIcon.addEventListener('mouseleave', () => {
-			this.hintImage.classList.add(this.hintImage.classList.remove(`${this.hintImage.classList[0]}--visible`));
-		});
+		this.hintIcon.onclick = () => {
+			for (let i = 0; i < this.hintImageContainerList.length; i++) {
+				this.show(i);
+			}
+		};
+		for (let i = 0; i < this.hintImageCloseList.length; i++) {
+			this.hintImageCloseList[i].onclick = () => {
+				this.close(i);
+			};
+		}
+	}
+
+	show(i) {
+		this.hintImageContainerList[i].classList.add(`${this.hintImageContainerList[i].classList[0]}--visible`);
+	}
+
+	close(i) {
+		this.hintImageContainerList[i].classList.remove(`${this.hintImageContainerList[i].classList[0]}--visible`);
 	}
 }
