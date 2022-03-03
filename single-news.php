b3b245bcf65b3b484bd017facead0383b105c52a
Alexandre Giroux <alex@instynctweb.com>
2022-03-03 16:19:40 -0500

    News & Singles News Mobile CSS

--- single-news.php
+++ single-news.php
@@ -14,14 +14,26 @@ get_header(); ?>
 
 <section class="article-back" data-wolfpack-section>
     <div class="article-back__container">
-        <a class="article-back__button" href="<?= $hero['back']['url'] ?>" target="<?= $hero['back']['target'] ?>"><?= $hero['back']['title'] ?></a>
+        <div class="article-back__button-container">
+            <a class="article-back__button" href="<?= $hero['back']['url'] ?>" target="<?= $hero['back']['target'] ?>">
+                <svg class="article-back__button-icon" xmlns="http://www.w3.org/2000/svg" width="14.515" height="13.384" viewBox="0 0 14.515 13.384">
+                    <g id="Groupe_788" data-name="Groupe 788" transform="translate(13.515 1.414) rotate(90)" opacity="0.3">
+                        <path id="Tracé_3" data-name="Tracé 3" d="M0,0,5.278,5.278,10.555,0" transform="translate(0 7.237)" fill="none" stroke="#051c2f" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" />
+                        <line id="Ligne_1" data-name="Ligne 1" y1="12.515" transform="translate(5.278 0)" fill="none" stroke="#051c2f" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" />
+                    </g>
+                </svg>
+                <span class="article-back__button-content">
+                    <?= $hero['back']['title'] ?>
+                </span>
+            </a>
+        </div>
     </div>
 </section>
 
 <?php $content = get_field('content'); ?>
 <section class="article-content" data-wolfpack-section>
     <div class="article-content__container">
-        <h1 class="article-container__title"><?= $hero['title'] ?></h1>
+        <h1 class="article-content__title"><?= $hero['title'] ?></h1>
         <div class="article-content__content">
             <?= $content ?>
         </div>
