1b0d82a3d6eec8fb1b775179e016f306d08046a1
Alexandre Giroux <alex@instynctweb.com>
2022-04-22 09:49:34 -0400

    Function to create new customer in Stripe

--- functions.php
+++ functions.php
@@ -68,10 +68,10 @@ function gf_replace_charge_description($description, $strings, $entry, $submissi
     return $description;
 }
 
-add_filter( 'gform_stripe_customer_id', function ( $customer_id, $feed, $entry, $form ) {
-    if ( rgars( $feed, 'meta/transactionType' ) == 'product') {
+add_filter('gform_stripe_customer_id', function ($customer_id, $feed, $entry, $form) {
+    if (rgars($feed, 'meta/transactionType') == 'product') {
         $customer_meta = array();
-        $metadata_field = rgars( $feed, 'meta/metaData' );
+        $metadata_field = rgars($feed, 'meta/metaData');
 
         foreach ($metadata_field as $metadata) {
             if ($metadata['custom_key'] == 'first_name') {
@@ -82,31 +82,74 @@ add_filter( 'gform_stripe_customer_id', function ( $customer_id, $feed, $entry, 
                 $phone = $metadata['value'];
             }
         }
-        
-        if ( ! empty( $first_name ) ) {
-            $customer_meta['first_name'] = gf_stripe()->get_field_value( $form, $entry, $first_name );
+
+        if (!empty($first_name)) {
+            $customer_meta['first_name'] = gf_stripe()->get_field_value($form, $entry, $first_name);
         }
-		
-		if ( ! empty( $last_name ) ) {
-            $customer_meta['last_name'] = gf_stripe()->get_field_value( $form, $entry, $last_name );
+
+        if (!empty($last_name)) {
+            $customer_meta['last_name'] = gf_stripe()->get_field_value($form, $entry, $last_name);
         }
-		
-		if ( ! empty( $phone ) ) {
-            $customer_meta['phone'] = gf_stripe()->get_field_value( $form, $entry, $phone );
+
+        if (!empty($phone)) {
+            $customer_meta['phone'] = gf_stripe()->get_field_value($form, $entry, $phone);
         }
-		
-        $customer = gf_stripe()->create_customer( $customer_meta, $feed, $entry, $form );
- 
+
+        if (empty($customer_id)) {
+            $response = gf_stripe()->get_stripe_js_response();
+            if (!empty($response->id) && substr($response->id, 0, 3) === 'pi_') {
+                try {
+                    $intent = \Stripe\PaymentIntent::retrieve($response->id);
+                    if (!empty($intent->customer)) {
+                        gf_stripe()->log_debug('gform_stripe_customer_id: PaymentIntent already has customer; ' . print_r($intent->customer, true));
+
+                        return is_object($intent->customer) ? $intent->customer->id : $intent->customer;
+                    }
+                } catch (\Exception $e) {
+                    gf_stripe()->log_debug('gform_stripe_customer_id: unable to get PaymentIntent; ' . $e->getMessage());
+                }
+            }
+
+            $customer_params = array();
+
+            $response = gf_stripe()->get_stripe_js_response();
+            if (!empty($response->id) && substr($response->id, 0, 3) === 'pi_') {
+                try {
+                    $intent = \Stripe\PaymentIntent::retrieve($response->id);
+                    if (!empty($intent->customer)) {
+                        gf_stripe()->log_debug('gform_stripe_customer_id: PaymentIntent already has customer; ' . print_r($intent->customer, true));
+
+                        return is_object($intent->customer) ? $intent->customer->id : $intent->customer;
+                    }
+                } catch (\Exception $e) {
+                    gf_stripe()->log_debug('gform_stripe_customer_id: unable to get PaymentIntent; ' . $e->getMessage());
+                }
+            }
+
+            $email_field = rgars($feed, 'meta/receipt_field');
+            if (!empty($email_field) && strtolower($email_field) !== 'do not send receipt') {
+                $customer_params['email'] = gf_stripe()->get_field_value($form, $entry, $email_field);
+            }
+
+            $customer_params['name'] = rgar($entry, '8.3') . ' ' . gf_stripe()->get_field_value($form, $entry, $first_name) . ' ' . gf_stripe()->get_field_value($form, $entry, $last_name);
+            gf_stripe()->log_debug(__METHOD__ . '(): Name: ' . $customer_params['name']);
+
+            $customer = gf_stripe()->create_customer($customer_meta, $feed, $entry, $form);
+            $customer_id = $customer->id;
+            gf_stripe()->log_debug('gform_stripe_customer_id: Returning Customer ID: ' . $customer_id);
+        }
+
         return $customer->id;
     }
- 
-    return $customer_id;
-}, 10, 4 );
 
-add_filter( 'gform_currencies', 'add_cad_currency' );
-function add_cad_currency( $currencies ) {
+    return $customer_id;
+}, 10, 4);
+
+add_filter('gform_currencies', 'add_cad_currency');
+function add_cad_currency($currencies)
+{
     $currencies['CAD'] = array(
-		'name'               => __( 'Canadian Dollar', 'gravityforms' ),
+        'name'               => __('Canadian Dollar', 'gravityforms'),
         'symbol_left'        => '',
         'symbol_right'       => '',
         'symbol_padding'     => ' ',
@@ -114,8 +157,8 @@ function add_cad_currency( $currencies ) {
         'decimal_separator'  => ',',
         'decimals'           => 2
     );
-  
+
     return $currencies;
 }
 
-add_filter( 'gform_confirmation_anchor', '__return_true' );
+add_filter('gform_confirmation_anchor', '__return_true');
